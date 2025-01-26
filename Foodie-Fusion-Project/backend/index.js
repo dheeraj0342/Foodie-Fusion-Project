4815c7841a668590ae95d53580aaabea933b3fa4
dheeraj singh <dheerajsingh86696@gmail.com>
2025-01-26 17:31:26 +0530

    CREATED: Basic Server

---
+++ Foodie-Fusion-Project/backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
